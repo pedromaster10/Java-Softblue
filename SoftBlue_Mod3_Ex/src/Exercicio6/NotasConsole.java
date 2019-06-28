5050cdd6b3f97a18efe24629af22c38eb72d44c2
Pedro Silva <pedro.nbk@gmail.com>
2019-06-28 18:12:33 -0300

    adição de varios exercicios

---
+++ SoftBlue_Mod3_Ex/src/Exercicio6/NotasConsole.java
@@ -0,0 +1,17 @@
+package Exercicio6;
+
+public class NotasConsole {
+	
+	public static void main(String [] args) {
+	
+	double TotalNotas;
+	int contaNotas;
+	double nota = 0;
+	
+	while (nota != -1) {
+		System.out.println("Digite uma nota: ");
+		nota  = Console.readDouble();
+	}
+	
+	}
+}
