1425c6b01daea31cc1a869adc04bacc3d10a67b8
Pedro Silva <pedro.nbk@gmail.com>
2019-07-08 23:25:23 -0300

    exercicio 7 modulo 3

--- SoftBlue_Mod3_Ex/src/Exercicio6/NotasConsole.java
+++ SoftBlue_Mod3_Ex/src/Exercicio6/NotasConsole.java
@@ -4,13 +4,22 @@ public class NotasConsole {
 	
 	public static void main(String [] args) {
 	
-	double TotalNotas;
-	int contaNotas;
-	double nota = 0;
+	double totalNotas = 0.0;
+	int contaNotas = 0;
+	double nota = 0.0;
 	
 	while (nota != -1) {
 		System.out.println("Digite uma nota: ");
 		nota  = Console.readDouble();
+		contaNotas++;
+		
+		if (nota < 0 || nota > 10) {
+			System.out.println("Nota invalida!");
+			continue;
+		}
+		//acumula o valor da soma das notas
+		totalNotas += nota;
+		
 	}
 	
 	}
